db3c57cfc2d6f5fa343b584e7d523ead37526db1
David Amorim <davidfilipeamorim@gmail.com>
2022-04-24 13:39:15 +0100

    Update dependencies

--- main/build.gradle.kts
+++ main/build.gradle.kts
@@ -12,20 +12,20 @@ dependencies {
   implementation("com.github.davidffa:lavaplayer-natives-fork:1.0.0")
   implementation("com.github.walkyst.JAADec-fork:jaadec-ext-aac:0.1.3")
   implementation("org.mozilla:rhino-engine:1.7.14")
-  implementation("org.slf4j:slf4j-api:1.7.35")
+  implementation("org.slf4j:slf4j-api:1.7.36")
 
-  api("org.apache.httpcomponents:httpclient:4.5.10")
-  implementation("commons-io:commons-io:2.6")
+  api("org.apache.httpcomponents:httpclient:4.5.13")
+  implementation("commons-io:commons-io:2.11.0")
 
-  api("com.fasterxml.jackson.core:jackson-core:2.10.0")
-  api("com.fasterxml.jackson.core:jackson-databind:2.10.0")
+  api("com.fasterxml.jackson.core:jackson-core:2.13.2")
+  api("com.fasterxml.jackson.core:jackson-databind:2.13.2.2")
 
   implementation("org.jsoup:jsoup:1.14.3")
   implementation("net.iharder:base64:2.3.9")
 
-  testImplementation("org.codehaus.groovy:groovy:3.0.8")
-  testImplementation("org.spockframework:spock-core:2.0-groovy-2.5")
-  testImplementation("ch.qos.logback:logback-classic:1.2.10")
+  testImplementation("org.codehaus.groovy:groovy:3.0.10")
+  testImplementation("org.spockframework:spock-core:2.1-groovy-2.5")
+  testImplementation("ch.qos.logback:logback-classic:1.2.11")
   testImplementation("com.sedmelluq:lavaplayer-test-samples:1.3.11")
 }
 
