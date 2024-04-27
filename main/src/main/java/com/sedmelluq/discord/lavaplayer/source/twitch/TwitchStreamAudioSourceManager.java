7e833a472db99905ab33f1fde25c912bd7db7f61
Kostya <gygi4@gusic.xyz>
2024-04-27 15:05:48 +0300

    Fix Twitch sometimes throwing on initialization

--- main/src/main/java/com/sedmelluq/discord/lavaplayer/source/twitch/TwitchStreamAudioSourceManager.java
+++ main/src/main/java/com/sedmelluq/discord/lavaplayer/source/twitch/TwitchStreamAudioSourceManager.java
@@ -24,6 +24,8 @@ import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.DataInput;
 import java.io.DataOutput;
@@ -44,6 +46,7 @@ import static com.sedmelluq.discord.lavaplayer.tools.FriendlyException.Severity.
  * Audio source manager which detects Twitch tracks by URL.
  */
 public class TwitchStreamAudioSourceManager implements AudioSourceManager, HttpConfigurable {
+  private static final Logger log = LoggerFactory.getLogger(TwitchStreamAudioSourceManager.class);
   private static final String STREAM_NAME_REGEX = "^https://(?:www\\.|go\\.|m\\.)?twitch.tv/([^/]+)$";
   private static final Pattern streamNameRegex = Pattern.compile(STREAM_NAME_REGEX);
 
@@ -53,7 +56,14 @@ public class TwitchStreamAudioSourceManager implements AudioSourceManager, HttpC
 
   public TwitchStreamAudioSourceManager() {
     httpInterfaceManager = HttpClientTools.createDefaultThreadLocalManager();
-    initRequestHeaders();
+
+    try {
+        initRequestHeaders();
+    } catch (FriendlyException e) {
+        log.warn("Failed to fetch Twitch source keys. " +
+            "Key fetching will be retried upon loading a Twitch track. " +
+            "This warning can be ignored unless the issue persists.", e);
+    }
   }
 
   public String getClientId() {
@@ -72,9 +82,12 @@ public class TwitchStreamAudioSourceManager implements AudioSourceManager, HttpC
   @Override
   public AudioItem loadItem(AudioPlayerManager manager, AudioReference reference) {
     String streamName = getChannelIdentifierFromUrl(reference.identifier);
+
     if (streamName == null) {
       return null;
     }
+
+    initRequestHeaders();
 
     JsonBrowser channelInfo = fetchStreamChannelInfo(streamName).get("data").get("user");
 
@@ -84,7 +97,7 @@ public class TwitchStreamAudioSourceManager implements AudioSourceManager, HttpC
       String title = channelInfo.get("lastBroadcast").get("title").text();
 
       final String thumbnail = String.format(
-            "https://static-cdn.jtvnw.net/previews-ttv/live_user_%s-440x248.jpg",
+            TwitchConstants.TWITCH_IMAGE_PREVIEW_URL,
             streamName.toLowerCase(Locale.ROOT)
       );
 
@@ -192,23 +205,33 @@ public class TwitchStreamAudioSourceManager implements AudioSourceManager, HttpC
   }
 
   private void initRequestHeaders() {
-    try (HttpInterface httpInterface = getHttpInterface()) {
-      HttpGet get = new HttpGet("https://www.twitch.tv");
-      get.setHeader("Accept", "text/html");
-      CloseableHttpResponse response = httpInterface.execute(get);
-      HttpClientTools.assertSuccessWithContent(response, "twitch main page");
-
-      String responseText = EntityUtils.toString(response.getEntity());
-      twitchClientId = DataFormatTools.extractBetween(responseText, "clientId=\"", "\"");
-
-      for (Header header : response.getAllHeaders()) {
-        if (header.getName().contains("Set-Cookie") && header.getValue().contains("unique_id=")) {
-          twitchDeviceId = DataFormatTools.extractBetween(header.toString(), "unique_id=", ";");
-        }
+      if (twitchClientId != null) {
+          return;
       }
-    } catch (IOException e) {
-      throw new FriendlyException("Loading Twitch main page failed.", SUSPICIOUS, e);
-    }
+
+      synchronized (this) {
+          if (twitchClientId != null) {
+              return;
+          }
+
+          try (HttpInterface httpInterface = getHttpInterface()) {
+              HttpGet get = new HttpGet(TwitchConstants.TWITCH_URL);
+              get.setHeader("Accept", "text/html");
+              CloseableHttpResponse response = httpInterface.execute(get);
+              HttpClientTools.assertSuccessWithContent(response, "twitch main page");
+
+              String responseText = EntityUtils.toString(response.getEntity());
+              twitchClientId = DataFormatTools.extractBetween(responseText, "clientId=\"", "\"");
+
+              for (Header header : response.getAllHeaders()) {
+                  if (header.getName().contains("Set-Cookie") && header.getValue().contains("unique_id=")) {
+                        twitchDeviceId = DataFormatTools.extractBetween(header.toString(), "unique_id=", ";");
+                  }
+              }
+          } catch (IOException e) {
+              throw new FriendlyException("Loading Twitch main page failed.", SUSPICIOUS, e);
+          }
+      }
   }
 
   @Override
