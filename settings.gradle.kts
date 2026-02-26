74e326ab91e9762ef607f85963fba5a957ef0ce9
Gygi4 <gygi4@gusic.xyz>
2026-02-26 15:36:57 +0200

    Update lavaplayer natives

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -50,7 +50,7 @@ fun VersionCatalogBuilder.common() {
 }
 
 fun VersionCatalogBuilder.others() {
-    library("lavaplayer-natives-fork", "com.github.davidffa", "lavaplayer-natives-fork").version("1.0.12")
+    library("lavaplayer-natives-fork", "com.github.Gygi4", "lavaplayer-natives-fork").version("1.0.6")
     library("ibxm-fork", "com.github.walkyst", "ibxm-fork").version("a75")
     library("rhino-engine", "org.mozilla", "rhino-engine").version("1.7.15")
 }
